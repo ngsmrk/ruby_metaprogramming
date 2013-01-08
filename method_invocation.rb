9b180838fd1ad54631598dc0b2e0920e69e5fad5
ngsmrk <ngsmrk@gmail.com>
2013-01-08 18:53:24 +0000

    Method invocation test now works

--- method_invocation.rb
+++ method_invocation.rb
@@ -1,24 +1,25 @@
 class MyClass
-  def public_method
-    "Public method called"
+  def do_something
+    puts "Public method called"
   end
   
   private
   
-  def private_method
+  def do_something_private
     "Private method called"
   end  
 end
 
 class MyClassTest < Test::Unit::TestCase
   def test_getting_public_method
-    method = MyClass.public_method(:public_method)
+
+    method = MyClass.new.method(:do_something)
     assert_equal Method, method.class
   end
   
   def test_getting_private_method_raises_error
     assert_raise(NameError) do
-      MyClass.public_method(:private_method)      
+      MyClass.new.public_method(:do_something_private)      
     end
   end  
 end
