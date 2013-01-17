2308bc2fbfd071eb237c3cff947160fedb5d22ec
ngsmrk <ngsmrk@gmail.com>
2013-01-17 18:28:33 +0000

    Added dynamic invocation tests

--- method_invocation.rb
+++ method_invocation.rb
@@ -1,26 +1,35 @@
 class MyClass
   def do_something
-    puts "Public method called"
+    "Public method called"
   end
-  
+
   private
   
   def do_something_private
     "Private method called"
-  end  
+  end
 end
 
 class MyClassTest < Test::Unit::TestCase
   def test_getting_public_method
 
-    method = MyClass.new.method(:do_something)
+    method = MyClass.new.public_method(:do_something)
     assert_equal Method, method.class
+    assert_equal "Public method called", method.call
   end
   
   def test_getting_private_method_raises_error
     assert_raise(NameError) do
-      MyClass.new.public_method(:do_something_private)      
+      MyClass.new.public_method(:do_something_private)
     end
-  end  
-end
+  end
 
+  def test_getting_unbound_method
+
+    method = MyClass.instance_method(:do_something)
+    assert_equal UnboundMethod, method.class
+
+    bound_method = method.bind(MyClass.new)
+    assert_equal "Public method called", bound_method.call    
+  end
+end
