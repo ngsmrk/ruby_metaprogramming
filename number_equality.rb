30102b183e460fdd9bc0dd7e64748c1f4aeb9b5d
ngsmrk <ngsmrk@gmail.com>
2013-03-02 12:18:20 +0000

    Added equality tests

--- number_equality.rb
+++ number_equality.rb
@@ -1,20 +1,21 @@
 class NumberEqualityTest < Test::Unit::TestCase
 	def test_integer_equal
-		assert_equal true,  1 ==   1, "Integer did not match using =="
-		assert_equal true,  1 ===  1, "Integer did not match using ==="
-		assert_equal true,  1.eql?(1), "Integer did not match using eql?"
-		assert_equal true,  1.equal?(1), "Integer did not match using equal?"						
+		assert_equal true,  1 ==   1, "Integer should match using =="
+		assert_equal true,  1 ===  1, "Integer should match using ==="
+		assert_equal true,  1.eql?(1), "Integer should match using eql?"
+		assert_equal true,  1.equal?(1), "Integer should match using equal?"						
 	end
 	def test_float_equal
-		assert_equal true,  1.0 ==   1.0, "Float did not match using =="
-		assert_equal true,  1.0 ===  1.0, "Float did not match using ==="
-		assert_equal true,  1.0.eql?(1.0), "Float did not match using eql?"
-		assert_equal false, 1.0.equal?(1.0), "Float unexpectedly matched using equal?"						
+		assert_equal true,  1.0 ==   1.0, "Float should match using =="
+		assert_equal true,  1.0 ===  1.0, "Float should match using ==="
+		assert_equal true,  1.0.eql?(1.0), "Float should match using eql?"
+		assert_equal false, 1.0.equal?(1.0), "Float should not match using equal?"						
 	end
 	def test_mixed_equal
-		assert_equal true,  1 ==   1.0, "Integer and Float did not match using =="
-		assert_equal true,  1 ===  1.0, "Integer and Float did not match using ==="
-		assert_equal false, 1.eql?(1.0), "Integer and Float unexpectedly matched using eql?"
-		assert_equal false, 1.equal?(1.0), "Integer and Float unexpectedly matched using equal?"						
+		assert_equal true,  1 ==   1.0, "Integer and Float should match using =="
+		assert_equal true,  1 ===  1.0, "Integer and Float should match using === because of type conversion"
+		assert_equal false,  1 ===  1.5, "Integer and Float should not match using === despite type conversion"		
+		assert_equal false, 1.eql?(1.0), "Integer and Float should not match using eql?"
+		assert_equal false, 1.equal?(1.0), "Integer and Float should not match using equal?"						
 	end	
 end
--- string_equality.rb
+++ string_equality.rb
@@ -1,8 +1,8 @@
 class StringEqualityTest < Test::Unit::TestCase
 	def test_equal
-		assert_equal true, "x" ==   "x", "String did not match using =="
-		assert_equal true, "x" ===  "x", "String did not match using ==="
-		assert_equal true, "x".eql?("x"), "String did not match using eql?"
-		assert_equal false, "x".equal?("x"), "String unexpectedly matched using equal?"						
+		assert_equal true, "x" ==   "x", "String should match using =="
+		assert_equal true, "x" ===  "x", "String should match using ==="
+		assert_equal true, "x".eql?("x"), "String should match using eql?"
+		assert_equal false, "x".equal?("x"), "String should not match using equal?"						
 	end
 end
