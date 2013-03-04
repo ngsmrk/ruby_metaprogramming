be168d36c8100e5f673f749f068208b73c6afb32
ngsmrk <ngsmrk@gmail.com>
2013-03-04 09:19:52 +0000

    More object equality tests

--- object_equality.rb
+++ object_equality.rb
@@ -1,3 +1,6 @@
+class TestClass
+end
+
 class ObjectEqualityTest < Test::Unit::TestCase
 	def test_equal
 		assert_equal false, Object.new ==   Object.new, "Object should not match using =="
@@ -5,11 +8,20 @@ class ObjectEqualityTest < Test::Unit::TestCase
 		assert_equal false, Object.new.eql?(Object.new), "Object should not match using eql?"
 		assert_equal false, Object.new.equal?(Object.new), "Object should not match using equal?"						
 	end
+
 	def test_equal_using_variables
 		x = Object.new
 		assert_equal true, x ==   x,   "Object variable should match using =="
 		assert_equal true, x ===  x,   "Object variable should match using ==="
 		assert_equal true, x.eql?(x),  "Object variable should match using eql?"
 		assert_equal true, x.equal?(x),"Object variable should match using equal?"						
-	end	
+	end
+
+	def test_equal_using_classes
+		x = TestClass.new
+		assert_equal true, x ==   x,   "Object variable should match using =="
+		assert_equal true, x ===  x,   "Object variable should match using ==="
+		assert_equal true, x.eql?(x),  "Object variable should match using eql?"
+		assert_equal true, x.equal?(x),"Object variable should match using equal?"						
+	end		
 end
