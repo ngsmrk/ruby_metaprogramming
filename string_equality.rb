adc550006a0bcc261e10e3eaaa22f27376a66dcd
ngsmrk <ngsmrk@gmail.com>
2013-03-26 16:27:03 +0000

    Moved equality and other tests to separate project

--- string_equality.rb
+++
@@ -1,8 +0,0 @@
-class StringEqualityTest < Test::Unit::TestCase
-	def test_equal
-		assert_equal true, "x" ==   "x", "String should match using =="
-		assert_equal true, "x" ===  "x", "String should match using ==="
-		assert_equal true, "x".eql?("x"), "String should match using eql?"
-		assert_equal false, "x".equal?("x"), "String should not match using equal?"						
-	end
-end
